6ca611877606933e4d280785cc615290215acc6d
Luis Enrique <luizhuaman94@gmail.com>
2026-06-30 02:01:29 -0500

    feat: add interface generic for service interfaces

--- src/main/java/com/mitocode/service/IMedicService.java
+++ src/main/java/com/mitocode/service/IMedicService.java
@@ -2,18 +2,6 @@ package com.mitocode.service;
 
 import com.mitocode.model.Medic;
 
-import java.util.List;
-
-public interface IMedicService {
-
-    Medic save(Medic medic);
-
-    Medic update(Integer id, Medic medic);
-
-    List<Medic> findAll();
-
-    Medic findById(Integer id);
-
-    void delete(Integer id);
+public interface IMedicService extends ICRUD<Medic, Integer>{
 
 }
--- src/main/java/com/mitocode/service/IPatientService.java
+++ src/main/java/com/mitocode/service/IPatientService.java
@@ -2,21 +2,6 @@ package com.mitocode.service;
 
 import com.mitocode.model.Patient;
 
-import java.util.List;
-
-public interface IPatientService {
-
-    Patient save(Patient patient);
-
-    Patient update(Integer id, Patient patient);
-
-    List<Patient> findAll();
-
-    Patient findById(Integer id);
-
-    void delete(Integer id);
-
-    //Patient validAndSave(Patient patient);
-
+public interface IPatientService extends ICRUD<Patient, Integer>{
 
 }
